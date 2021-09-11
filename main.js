85fc9dce298ca130ea17663dfd9841755684bf42
amoungus123 <76024871+amoungus123@users.noreply.github.com>
2021-09-10 20:43:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t2wQ4hGGM/model.json', modelLoaded)
+}
+
+function modelLoaded() {
+    console.log("😀😀Model (❁´◡`❁) Loaded!😀😀");
+}
+
+function draw() {
+    image(video, 0, 0, 500, 500);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results ) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById('result_object_name').innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
+function modelLoaded() {
+    console.log("😀😀Model (❁´◡`❁) Loaded!😀😀");
+}
